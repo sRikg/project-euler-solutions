695c0f3b8f23085711032b36351791d0f082d8e6
Srik Gorthy <sskanthgorthy@gmail.com>
2023-01-18 15:11:13 -0600

    Update Q0001.R

--- Q0001.R
+++ Q0001.R
@@ -1,16 +1,29 @@
+# Create an array that contains all integers from 1 to 999
 array <- 1:999
 
+# Define a function called "eliminator_3_5" that takes in a single argument, "n"
 eliminator_3_5 <- function(n){
+  # Check if the input "n" is divisible by 3
   if(n %% 3 == 0){
+    # If so, return the value of "n"
     return(n)
   } else {
+    # If "n" is not divisible by 3, check if it is divisible by 5
     if (n %% 5 == 0){
+      # If so, return the value of "n"
       return(n)
     } else {
+      # If "n" is not divisible by 3 or 5, return "0"
       return(0)
     }
   }
 }
 
+# Use the sapply function to apply the eliminator_3_5 function to every element of the "array"
 array_eliminated <- sapply(array, eliminator_3_5)
-print(sum(array_eliminated))
+
+# Calculate the sum of all the elements of the "array_eliminated" array
+result <- sum(array_eliminated)
+
+# Print the result
+print(result)
